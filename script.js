63e4e506e18b25151362894816541053c243feab
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-18 07:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let num = parseInt(n);
+	let sum = 1; 
+	for(let i = 1; i <= num; i++){
+		sum = sum * i;
+	}
+	let str = String(sum).split('');
+	let count = 0;
+	for(let i = 0; i < str.length; i++){
+		if(str[i] == '0'){
+			count++;
+		}
+	}
+	return count;
 }
 
 const input = prompt("Enter a number");
